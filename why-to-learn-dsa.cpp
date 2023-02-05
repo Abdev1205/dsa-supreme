becbc0ba8f05696c2b69de87e218f0fdd8929836
Abhay1205Mishra <97285855+Abhay1205Mishra@users.noreply.github.com>
2023-02-05 14:06:14 +0530

    intial commit

---
+++ why-to-learn-dsa.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+using namespace std;
+int main() {
+    cout<<"I want to make my parent proud on me";
+    cout << "I will try to get good package from my college and make them feel proud";
+}
